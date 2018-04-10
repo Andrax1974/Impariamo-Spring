4f69d078c12ecaa50b9675685fe104a8503ed013
Nicola La Rocca <nicola.larocca@xantrix.it>
2018-04-10 19:13:07 +0200

    Commit Test Web Services

---
+++ Corso1/TestWebServices/src/main/java/com/xantrix/webapp/TestWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.xantrix.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWebServicesApplication.class, args);
+	}
+}
